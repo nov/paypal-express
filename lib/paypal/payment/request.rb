b93abf163dc4c54bc4e710c2a9ba092ac87e8ca4
nov matake <nov@matake.jp>
2011-02-07 17:58:54 +0900

    notify url support

--- lib/paypal/payment/request.rb
+++ lib/paypal/payment/request.rb
@@ -1,13 +1,15 @@
 module Paypal
   module Payment
     class Request < Base
-      attr_optional :amount, :action, :currency_code, :billing_type, :billing_agreement_description
+      attr_optional :amount, :action, :currency_code, :notify_url, :billing_type, :billing_agreement_description
 
       def to_params(index = 0)
         {
           :"PAYMENTREQUEST_#{index}_PAYMENTACTION" => self.action,
           :"PAYMENTREQUEST_#{index}_AMT" => Util.formatted_amount(self.amount),
           :"PAYMENTREQUEST_#{index}_CURRENCYCODE" => self.currency_code,
+          :"PAYMENTREQUEST_#{index}_DESC" => self.description,
+          :"PAYMENTREQUEST_#{index}_NOTIFYURL" => self.notify_url,
           :"L_BILLINGTYPE#{index}" => self.billing_type,
           :"L_BILLINGAGREEMENTDESCRIPTION#{index}" => self.billing_agreement_description
         }.delete_if do |k, v|
