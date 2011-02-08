66e858de47487ac82e3006c96f6834dcc47188f9
nov matake <nov@matake.jp>
2011-02-08 14:57:23 +0900

    exception spec

--- spec/paypal/exception_spec.rb
+++ spec/paypal/exception_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper.rb'
 
-describe Paypal::Exception::HttpError do
+describe Paypal::HttpError do
   it 'should have code, message and body' do
-    error = Paypal::Exception::HttpError.new(400, 'BadRequest', 'You are bad man!')
+    error = Paypal::HttpError.new(400, 'BadRequest', 'You are bad man!')
+    error.code.should == 400
+    error.message.should == 'BadRequest'
+    error.body.should == 'You are bad man!'
   end
 end
 
-describe Paypal::Exception::APIError do
+describe Paypal::APIError do
+  it 'should have raw response' do
+    error = Paypal::APIError.new({:error => 'ERROR!!'})
+    error.response.should == {:error => 'ERROR!!'}
+  end
 end
