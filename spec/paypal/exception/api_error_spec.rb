b0271a6ae06a4d14a30547b2c84542a0d7426cdd
nov matake <nov@matake.jp>
2011-03-04 19:11:38 +0900

    error spec update

--- spec/paypal/exception/api_error_spec.rb
+++ spec/paypal/exception/api_error_spec.rb
@@ -1,73 +1,65 @@
 require 'spec_helper.rb'
 
 describe Paypal::Exception::APIError do
-  describe '.new' do
-    let :error do
-      Paypal::Exception::APIError.new response
+  let(:error) { Paypal::Exception::APIError.new(params) }
+
+  context 'when Hash is given' do
+    let :params do
+      {
+        :VERSION=>"66.0",
+        :TIMESTAMP=>"2011-03-03T06:33:51Z",
+        :CORRELATIONID=>"758ebdc546b9c",
+        :L_SEVERITYCODE0=>"Error",
+        :L_ERRORCODE0=>"10411",
+        :L_LONGMESSAGE0=>"This Express Checkout session has expired.  Token value is no longer valid.",
+        :BUILD=>"1741654",
+        :ACK=>"Failure",
+        :L_SHORTMESSAGE0=>"This Express Checkout session has expired."
+      }
     end
 
-    context 'when Hash is given' do
-      let :response do
-        {
-          :VERSION=>"66.0",
-          :TIMESTAMP=>"2011-03-03T06:33:51Z",
-          :CORRELATIONID=>"758ebdc546b9c",
-          :L_SEVERITYCODE0=>"Error",
-          :L_ERRORCODE0=>"10411",
-          :L_LONGMESSAGE0=>"This Express Checkout session has expired.  Token value is no longer valid.",
-          :BUILD=>"1741654",
-          :ACK=>"Failure",
-          :L_SHORTMESSAGE0=>"This Express Checkout session has expired."
-        }
+    describe '#subject' do
+      subject { error.response }
+      its(:raw) { should == params }
+      Paypal::Exception::APIError::Response.attribute_mapping.each do |key, attribute|
+        its(attribute) { should == params[key] }
       end
 
-      it 'should have Paypal::Exception::APIError::Response as response' do
-        Paypal::Exception::APIError::Response.attribute_mapping.each do |key, value|
-          error.response.send(value).should == response[key]
+      describe '#details' do
+        subject { error.response.details.first }
+        Paypal::Exception::APIError::Response::Detail.attribute_mapping.each do |key, attribute|
+          its(attribute) { should == params[:"L_#{key}0"] }
         end
-      end
-
-      it 'should have Paypal::Exception::APIError::Response::Detail as response.detail' do
-        detail = error.response.details.first
-        Paypal::Exception::APIError::Response::Detail.attribute_mapping.each do |key, value|
-          detail.send(value).should == response[:"L_#{key}0"]
-        end
-      end
-
-      it 'should have raw response as response.raw' do
-        error.response.raw.should == response
-      end
-
-      context 'when unknown params given' do
-        let :response do
-          {
-            :UNKNOWN => 'Unknown',
-            :L_UNKNOWN0 => 'Unknown Detail'
-          }
-        end
-
-        it 'should warn it and keep it only in response.raw' do
-          Paypal.logger.should_receive(:warn).with(
-            "Ignored Parameter (Paypal::Exception::APIError::Response): UNKNOWN=Unknown"
-          )
-          Paypal.logger.should_receive(:warn).with(
-            "Ignored Parameter (Paypal::Exception::APIError::Response::Detail): UNKNOWN=Unknown Detail"
-          )
-          error
-        end
-      end
-    end
-
-    context 'otherwise' do
-      let :response do
-        'Failure'
-      end
-
-      it 'should store response in raw format' do
-        error = Paypal::Exception::APIError.new response
-        error.response.should == response
       end
     end
   end
-  
+
+  context 'when unknown params given' do
+    let :params do
+      {
+        :UNKNOWN => 'Unknown',
+        :L_UNKNOWN0 => 'Unknown Detail'
+      }
+    end
+
+    it 'should warn' do
+      Paypal.logger.should_receive(:warn).with(
+        "Ignored Parameter (Paypal::Exception::APIError::Response): UNKNOWN=Unknown"
+      )
+      Paypal.logger.should_receive(:warn).with(
+        "Ignored Parameter (Paypal::Exception::APIError::Response::Detail): UNKNOWN=Unknown Detail"
+      )
+      error
+    end
+    describe '#response' do
+      subject { error.response }
+      its(:raw) { should == params }
+    end
+  end
+
+  context 'otherwise' do
+    subject { error }
+    let(:params) { 'Failure' }
+    its(:response) { should == params }
+  end
 end
--- spec/paypal/exception/http_error_spec.rb
+++ spec/paypal/exception/http_error_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper.rb'
 
 describe Paypal::Exception::HttpError do
-  it 'should have code, message and body' do
-    error = Paypal::Exception::HttpError.new(400, 'BadRequest', 'You are bad man!')
-    error.code.should == 400
-    error.message.should == 'BadRequest'
-    error.body.should == 'You are bad man!'
-  end
+  subject { Paypal::Exception::HttpError.new(400, 'BadRequest', 'You are bad man!') }
+  its(:code)    { should == 400 }
+  its(:message) { should == 'BadRequest' }
+  its(:body)    { should == 'You are bad man!' }
 end
