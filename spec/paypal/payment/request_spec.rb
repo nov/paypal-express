91c22e09b34b0321ca100489205682e1854b1b4f
Warlley <warlleyrezende@gmail.com>
2011-04-20 16:25:37 -0300

    Fixed items amount calc bug

--- spec/paypal/payment/request_spec.rb
+++ spec/paypal/payment/request_spec.rb
@@ -3,15 +3,20 @@ require 'spec_helper.rb'
 describe Paypal::Payment::Request do
   let :instant_request do
     Paypal::Payment::Request.new(
-      :amount => 20.5,
+      :amount => 23.8,
       :currency_code => :JPY,
       :description => 'Instant Payment Request',
       :notify_url => 'http://merchant.example.com/notify',
       :items => [{
         :quantity => 2,
         :name => 'Item1',
-        :description => 'Awesome Item!',
+        :description => 'Awesome Item 1!',
         :amount => 10.25
+      }, {
+        :quantity => 3,
+        :name => 'Item2',
+        :description => 'Awesome Item 2!',
+        :amount => 1.1        
       }]
     )
   end
@@ -26,7 +31,7 @@ describe Paypal::Payment::Request do
 
   describe '.new' do
     it 'should handle Instant Payment parameters' do
-      instant_request.amount.should == 20.5
+      instant_request.amount.should == 23.8
       instant_request.currency_code.should == :JPY
       instant_request.description.should == 'Instant Payment Request'
       instant_request.notify_url.should == 'http://merchant.example.com/notify'
@@ -42,15 +47,19 @@ describe Paypal::Payment::Request do
   describe '#to_params' do
     it 'should handle Instant Payment parameters' do
       instant_request.to_params.should == {
-        :PAYMENTREQUEST_0_AMT => "20.50",
+        :PAYMENTREQUEST_0_AMT => "23.80",
         :PAYMENTREQUEST_0_CURRENCYCODE => :JPY,
         :PAYMENTREQUEST_0_DESC => "Instant Payment Request", 
         :PAYMENTREQUEST_0_NOTIFYURL => "http://merchant.example.com/notify",
-        :PAYMENTREQUEST_0_ITEMAMT => "20.50",
+        :PAYMENTREQUEST_0_ITEMAMT => "23.80",
         :L_PAYMENTREQUEST_0_NAME0 => "Item1",
-        :L_PAYMENTREQUEST_0_DESC0 => "Awesome Item!",
+        :L_PAYMENTREQUEST_0_DESC0 => "Awesome Item 1!",
         :L_PAYMENTREQUEST_0_AMT0 => "10.25",
-        :L_PAYMENTREQUEST_0_QTY0 => 2
+        :L_PAYMENTREQUEST_0_QTY0 => 2,
+        :L_PAYMENTREQUEST_0_NAME1 => "Item2",
+        :L_PAYMENTREQUEST_0_DESC1 => "Awesome Item 2!",
+        :L_PAYMENTREQUEST_0_AMT1 => "1.10",
+        :L_PAYMENTREQUEST_0_QTY1 => 3
       }
     end
 
