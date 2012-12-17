38a3e176a514b38e74cc3abf653a02b34dd8fdb5
Stuart Coyle <stuart.coyle@gmail.com>
2012-12-17 13:55:28 +1000

    Whitespace

--- lib/paypal/express/request.rb
+++ lib/paypal/express/request.rb
@@ -14,11 +14,11 @@ module Paypal
           params[:NOSHIPPING] = 1
         end
         {
-          :solution_type  => :SOLUTIONTYPE,
-          :landing_page   => :LANDINGPAGE,
-          :email          => :EMAIL,
-          :brand          => :BRANDNAME,
-          :locale         => :LOCALECODE,
+          :solution_type => :SOLUTIONTYPE,
+          :landing_page  => :LANDINGPAGE,
+          :email         => :EMAIL,
+          :brand         => :BRANDNAME,
+          :locale        => :LOCALECODE
         }.each do |option_key, param_key|
           params[param_key] = options[option_key] if options[option_key]
         end
