50d33fabb640a13a42e28f7700e98cf0c0ad5370
Jarek Jeleniewicz <jarojele@gmail.com>
2014-12-05 12:08:44 +0100

    remove unnecessary self

--- lib/paypal.rb
+++ lib/paypal.rb
@@ -6,7 +6,7 @@ require 'rest_client'
 
 module Paypal
   attr_accessor :api_version
-  self.api_version = '88.0'
+  api_version = '88.0'
 
   ENDPOINT = {
     :production => 'https://www.paypal.com/cgi-bin/webscr',
