28d7cdf994c3920828a516b8a99100b84cba1eb0
Chan Hong Ming <hongming.chm@gmail.com>
2016-01-04 14:57:04 +0800

    Remove puts and renamed the params to the correct one

--- lib/paypal/nvp/transactions_response.rb
+++ lib/paypal/nvp/transactions_response.rb
@@ -4,21 +4,20 @@ module Paypal
       cattr_reader :attribute_mapping
       @@attribute_mapping = {
         :ACK => :ack,
+        :BUILD => :build,
         :CORRELATIONID => :correlation_id,
         :TIMESTAMP => :timestamp,
         :VERSION => :version,
         :CUSTOM => :custom,
-        :ERRORCODE => :error_code,
-        :SHORTMESSAGE => :short_message,
-        :LONGMESSAGE => :long_message,
-        :SEVERITYCODE => :severity_code,
-        :BUILD => :build
+        :L_ERRORCODE0 => :error_code,
+        :L_SHORTMESSAGE0 => :short_message,
+        :L_LONGMESSAGE0 => :long_message,
+        :L_SEVERITYCODE0 => :severity_code
       }
       attr_accessor *@@attribute_mapping.values
       attr_accessor :transactions
 
       def initialize(attributes = {})
-        puts attributes
         attrs = attributes.dup
 
         @@attribute_mapping.each do |key, value|
